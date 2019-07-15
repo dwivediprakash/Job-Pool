27e29e773cde6198360ee1b99c06e06e95d8682b
payal24898 <payal24898>
2019-07-15 14:06:14 +0530

    First Commit

---
+++ Website/feedback.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+$name=$_POST['name'];
+$feedback=$_POST['feedback'];
+include('DbConnection.php');
+$query='insert into feedback values ("'.$name.'","'.$feedback.'")';
+echo $query;
+mysqli_query($con,$query);
+$_SESSION['message']='Your feedback has been posted';
+header('location:index.php');
+?>
